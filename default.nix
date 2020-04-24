bcd827827d4da761daf10e16ccb25c349701edf6
Peter Jones <pjones@devalot.com>
2020-04-24 14:19:19 -0700

    Update build system, build with GHC 8.6.5 on NixOS 20.03

--- default.nix
+++ default.nix
@@ -1,2 +1,21 @@
-{ pkgs ? (import <nixpkgs> {}).pkgs }:
-pkgs.haskellPackages.callPackage ./clockdown.nix { }
+{ sources ? import ./nix/sources.nix
+, pkgs ? import sources.nixpkgs { }
+, nix-hs ? import sources.nix-hs { inherit pkgs; }
+, ghcide ? sources.ghcide-nix
+, ormolu ? sources.ormolu
+, ghc ? "default"
+}:
+
+nix-hs {
+  cabal = ./clockdown.cabal;
+  compiler = ghc;
+
+  overrides = lib: self: super: with lib; {
+    ghcide = import ghcide {};
+
+    ormolu = (import ormolu {
+      inherit (lib) pkgs;
+      ormoluCompiler = lib.compilerName;
+    }).ormolu;
+  };
+}
